66c6751976a141f42da813e744a7833b6c3ea077
hfcb <hfcb@hfcbdeiMac.local>
2017-06-23 16:22:06 +0800

    Initial Commit

---
+++ ContainerViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContainerViewController
+//
+//  Created by hfcb on 2017/6/23.
+//  Copyright © 2017年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContainerViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ContainerViewController
+//
+//  Created by hfcb on 2017/6/23.
+//  Copyright © 2017年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
